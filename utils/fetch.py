3766c071cf2dc9cdfcc2c5c0c86f58ce8ca04e90
Vladislav Atakhanov <eloh1mZee@gmail.com>
2023-12-18 19:06:33 +0600

    fix: timeout error

--- utils/fetch.py
+++ utils/fetch.py
@@ -1,7 +1,13 @@
 import aiohttp
+from ..exceptions import TimeoutError
+
+timeout = aiohttp.ClientTimeout(total=5)
 
 
 async def fetch(url: str, cookies: dict[str, str]):
-    async with aiohttp.ClientSession(cookies=cookies) as session:
-        async with session.get(url, timeout=10) as response:
-            return await response.text()
+    try:
+        async with aiohttp.ClientSession(cookies=cookies, timeout=timeout) as session:
+            async with session.get(url, timeout=timeout) as response:
+                return await response.text()
+    except:
+        raise TimeoutError
