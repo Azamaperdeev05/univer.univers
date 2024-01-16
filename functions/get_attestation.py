2f1beee62a127f9eeb7bbfd3747e4b0bea92afd5
Vladislav Atakhanov <eloh1mZee@gmail.com>
2024-01-16 14:44:27 +0600

    fix: attestation

--- functions/get_attestation.py
+++ functions/get_attestation.py
@@ -64,13 +64,13 @@ def _find_element_by_key(elements: list, predicate):
 
 def _join_marks(a: list[Mark], b: list[Mark]) -> list[Mark]:
     result: list[Mark] = []
-    is_active_setted = False
+    is_active_set = False
     for a_mark in a:
         b_mark = _find_element_by_key(b, lambda b_mark: a_mark.title == b_mark.title)
         result_mark = b_mark or a_mark
-        if not is_active_setted and a_mark.value == 0:
+        if not is_active_set and a_mark.value == 0:
             result_mark = ActiveMark(title=result_mark.title, value=result_mark.value)
-            is_active_setted = tuple
+            is_active_set = tuple
         result.append(result_mark)
     return result
 
@@ -107,6 +107,8 @@ async def _get_attestation_subjects(
 def _get_attestation_by_subject(
     attestations: list[Attestation], subject: str, subjects: list[str]
 ) -> Attestation:
+    if len(subjects) < 1:
+        return None
     for current_subject in subjects:
         if subject in current_subject:
             break
@@ -119,10 +121,19 @@ def _get_attestation_by_subject(
 def _join(
     attestations: list[Attestation], attendances: list[_Attendance], subjects: list[str]
 ):
+    no_attestation = len(attestations) < 1
     for attendance in attendances:
         attestation = _get_attestation_by_subject(
             attestations, attendance.subject, subjects
         )
+        if no_attestation:
+            new_attestation = [Mark(title, 0) for title, _ in attendance.summary]
+            attestation = Attestation(
+                subject=attendance.subject,
+                attestation=new_attestation + [Mark(title="", value=0)],
+                attendance=[],
+            )
+            attestations.append(attestation)
         if attestation is None:
             continue
         attestation.attestation = _join_marks(
