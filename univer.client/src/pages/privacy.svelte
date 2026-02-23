933c3588ff13b287f103246eadf1f2c21638a0e6
Azamat Perdeev <127682978+Azamaperdeev05@users.noreply.github.com>
2026-02-23 20:24:13 +0500

    Debug: Added diagnostic information to Privacy Policy page to troubleshoot empty content issue

--- univer.client/src/pages/privacy.svelte
+++ univer.client/src/pages/privacy.svelte
@@ -4,6 +4,7 @@
     import { _ } from "$lib/i18n/index.ts"
     import AppBar from "$lib/components/app-bar.svelte"
     import Loader from "$lib/components/loader.svelte"
+    import { Button } from "$lib/components/ui/button"
     import { useApi } from "$api"
 
     const api = useApi()
@@ -12,16 +13,23 @@
 
 <Page>
     {#snippet header()}
-        <AppBar title={_("privacy-policy")}>
-
-        </AppBar>
+        <AppBar title={_("privacy-policy")}></AppBar>
     {/snippet}
     <div class="content max-w-3xl mx-auto p-4">
-        {#if query.loading}
+        {#if query.loading && !query.data}
             <Loader />
         {:else if query.data}
             {@html query.data}
+        {:else if !query.loading && !query.data}
+            <p class="text-center text-muted-foreground p-10">
+                Деректер бос. Күй: {query.state}
+                <br />
+                <Button
+                    variant="outline"
+                    onclick={() => query.fetch()}
+                    class="mt-4">Қайта жүктеу</Button
+                >
+            </p>
         {/if}
     </div>
 </Page>
-
